a3b29a9d690c8a2927923a34a1ad65caef1e86db
Dominic Boulanger <dominic_boulanger@hotmail.com>
2019-01-23 00:07:29 -0500

    First iteration

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { Client, Attachment } = require('discord.js');
+const request = require('request');
+
 const config = require('./config');
 const utils = require('./utils');
 const giphy = require('./giphy');
 
 const EVENT_READY = 'ready';
 const EVENT_MESSAGE = 'message';
-
 
 const client = new Client();
 
@@ -17,6 +18,14 @@ client.on(EVENT_MESSAGE, message => {
     if (message.startsWith(config.discord.botCommandPrefix)) {
         let message = utils.parse(message.content);
         let sanitizedQuery = utils.sanitize(message);
+
+        request(giphy.getRandomFullUrlForQuery(sanitizedQuery), (err, res, body) => {
+            var response = JSON.parse(body);
+            const attachment = new Attachment(response.data.images.original.url);
+            message.channel.send(attachment);
+        });
+
+        
     }
 });
 
